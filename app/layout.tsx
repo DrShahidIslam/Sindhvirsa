d31d80d974680341518a0f8ce7b6c4c949e68f60
= <=>
2023-06-06 23:48:51 +0500

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    //<ClerkProvider>
+    <ClerkProvider>
       <html lang="en">      
         <body>{children}</body>
       </html>
-    //</ClerkProvider>
+    </ClerkProvider>
   );
 }
--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -4,7 +4,7 @@ import { createClient } from "next-sanity";
 
 
 export const client = createClient({
-  token: "skMywG8a2VnqEFESnsGKVwpndrSX8jTc2sDhugAiQhTyvx5KuP876ffefc8l9SgtlS3gkDhocwvanu9b1FxRrX0p2kZf8AiYtG6cqi5OXW2U0kIkerril3cTCcEFMdR9Y6bUNg0cKYs6fvAyiog5V6TRoYcSn5oFH8vslm31h4ZzS4yvUzsI",
+  token: process.env.SANITY_ACCESS_TOKEN,
   projectId: "hahpdko6",
   dataset: "production",
   apiVersion: "2023-06-06",
